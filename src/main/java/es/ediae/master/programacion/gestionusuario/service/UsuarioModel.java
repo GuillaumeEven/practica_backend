35d9b32385aeee25af9903d392f574c8ac292830
GuillaumeEven <guillaume@evendev.net>
2026-04-28 18:49:23 +0200

    feat(): create usuarioModel and usuariocontroller in progress

---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/service/UsuarioModel.java
@@ -0,0 +1,146 @@
+package es.ediae.master.programacion.gestionusuario.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import es.ediae.master.programacion.gestionusuario.entity.UsuarioEntity;
+
+public class UsuarioModel {
+
+    private Long id;
+    private String nick_usuario;
+    private String contrasena;
+    private LocalDateTime fecha_hora_creacion;
+    // private GeneroModel genero;
+    private String nombre;
+    private String primer_apellido;
+    private String segundo_apellido;
+    private LocalDate fecha_nacimiento;
+    private LocalTime hora_desayuno;
+
+    public UsuarioModel(String contrasena, LocalDateTime fecha_hora_creacion, LocalDate fecha_nacimiento, LocalTime hora_desayuno, Long id, String nick_usuario, String nombre, String primer_apellido, String segundo_apellido) {
+        this.contrasena = contrasena;
+        this.fecha_hora_creacion = fecha_hora_creacion;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.hora_desayuno = hora_desayuno;
+        this.id = id;
+        this.nick_usuario = nick_usuario;
+        this.nombre = nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNick_usuario() {
+        return nick_usuario;
+    }
+
+    public void setNick_usuario(String nick_usuario) {
+        this.nick_usuario = nick_usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public LocalDateTime getFecha_hora_creacion() {
+        return fecha_hora_creacion;
+    }
+
+    public void setFecha_hora_creacion(LocalDateTime fecha_hora_creacion) {
+        this.fecha_hora_creacion = fecha_hora_creacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimer_apellido() {
+        return primer_apellido;
+    }
+
+    public void setPrimer_apellido(String primer_apellido) {
+        this.primer_apellido = primer_apellido;
+    }
+
+    public String getSegundo_apellido() {
+        return segundo_apellido;
+    }
+
+    public void setSegundo_apellido(String segundo_apellido) {
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public LocalTime getHora_desayuno() {
+        return hora_desayuno;
+    }
+
+    public void setHora_desayuno(LocalTime hora_desayuno) {
+        this.hora_desayuno = hora_desayuno;
+    }
+
+    public static UsuarioModel fromEntity(UsuarioEntity usuarioEntity) {
+        return new UsuarioModel(
+                usuarioEntity.getContrasena(),
+                usuarioEntity.getFecha_hora_creacion(),
+                usuarioEntity.getFecha_nacimiento(),
+                usuarioEntity.getHora_desayuno(),
+                usuarioEntity.getId(),
+                usuarioEntity.getNick_usuario(),
+                usuarioEntity.getNombre(),
+                usuarioEntity.getPrimer_apellido(),
+                usuarioEntity.getSegundo_apellido());
+    }
+
+    // public static fromDto(UsuarioDTO usuarioDTO) {
+    //     return new UsuarioModel(
+    //             usuarioDTO.getContrasena(),
+    //             usuarioDTO.getFecha_hora_creacion(),
+    //             usuarioDTO.getFecha_nacimiento(),
+    //             usuarioDTO.getHora_desayuno(),
+    //             null,
+    //             usuarioDTO.getNick_usuario(),
+    //             usuarioDTO.getNombre(),
+    //             usuarioDTO.getPrimer_apellido(),
+    //             usuarioDTO.getSegundo_apellido());
+    // }
+
+    // public static UsuarioModel fromPostDto(UsuarioPostDTO usuarioPostDTO) {
+    //     return new UsuarioModel(
+    //             usuarioPostDTO.getContrasena(),
+    //             LocalDateTime.now(),
+    //             usuarioPostDTO.getFecha_nacimiento(),
+    //             usuarioPostDTO.getHora_desayuno(),
+    //             null,
+    //             usuarioPostDTO.getNick_usuario(),
+    //             usuarioPostDTO.getNombre(),
+    //             usuarioPostDTO.getPrimer_apellido(),
+    //             usuarioPostDTO.getSegundo_apellido());
+    // }
+
+
+}
