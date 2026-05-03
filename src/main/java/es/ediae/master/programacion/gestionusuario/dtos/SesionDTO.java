15812ff8f21ae35c4a94f9c7014f40f90c9a0192
GuillaumeEven <guillaume@evendev.net>
2026-05-04 00:59:34 +0200

    feat(): sesionDto validation

--- src/main/java/es/ediae/master/programacion/gestionusuario/dtos/SesionDTO.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/dtos/SesionDTO.java
@@ -2,7 +2,9 @@ package es.ediae.master.programacion.gestionusuario.dtos;
 
 public class SesionDTO {
 
+    @NotEmpty(message = "El nickUsuario no puede estar vacío")
     private String nickUsuario;
+    @NotEmpty(message = "La contraseña no puede estar vacía")
     private String contrasena;
 
     public String getNickUsuario() {
