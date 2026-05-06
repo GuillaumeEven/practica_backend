41259b43cb5c2a386a1348b200f495675b1e8452
GuillaumeEven <guillaume@evendev.net>
2026-05-06 19:02:10 +0200

    test(): tests class creation

---
+++ src/test/java/es/ediae/master/programacion/gestionusuario/service/DireccionServiceTest.java
@@ -0,0 +1,15 @@
+package es.ediae.master.programacion.gestionusuario.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import es.ediae.master.programacion.gestionusuario.repository.IDireccionRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class DireccionServiceTest {
+
+    @Mock
+    private IDireccionRepository direccionRepository;
+
+}
